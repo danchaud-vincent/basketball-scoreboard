fdb4ed6241dd49f042e0c8fd8c342930d37b1928
danchaud-vincent <vdanchaud@gmail.com>
2025-01-24 11:43:17 +0100

    update files

--- main.js
+++ main.js
@@ -2,15 +2,20 @@ let scoreHomeEl = document.getElementById("home-score-el");
 let scoreGuestEl = document.getElementById("guest-score-el");
 let timerEl = document.getElementById("timer");
 let periodEl = document.getElementById("period");
+let showEl = document.getElementById("showEl");
 
 
 let homeCount = 0;
 let guestCount = 0;
 let timerValue = null;
 let periodValue = 1;
+let winScore = 21;
 
+
+// Functions addOne, addTwo, addThree
 function addOne(id){
 
+    // check the id of the button to increment the right score
     if (id.includes("home") == true){
         // increment home's score
         homeCount += 1;
@@ -25,7 +30,8 @@ function addOne(id){
 }
 
 function addTwo(id){
-
+    
+    // check the id of the button to increment the right score
     if (id.includes("home") == true){
         // increment home's score
         homeCount += 2;
@@ -39,7 +45,8 @@ function addTwo(id){
 }
 
 function addThree(id){
-
+    
+    // check the id of the button to increment the right score
     if (id.includes("home") == true){
         // increment home's score
         homeCount += 3;
@@ -52,21 +59,42 @@ function addThree(id){
     } 
 }
 
+// Function win
+function win(){
+
+    if (homeCount>winScore){
+        console.log("The Home team Won the game") 
+        showEl.style.visibility = "visible";
+        showEl.innerHTML = "HOME's TEAM WINS THE GAME";
+        showEl.style.backgroundColor = "red";
+    }
+    else{
+        console.log("The Guest team Won the game") 
+        showEl.innerHTML = "GUEST's TEAM WINS THE GAME";
+    }
+}
+
 
 // NEW GAME function
 function startGame(){
 
+    // Get a reference to the last interval + 1
+    const interval_id = window.setInterval(function(){}, Number.MAX_SAFE_INTEGER);
+    
     // timer
     if (timerValue === null){
         // set timer
         timerValue = timer();
     }
     else{
-        // clear timer
-        clearInterval(timerValue);
-
+        // Clear any timeout/interval up to that id
+        for (let i = 1; i < interval_id; i++) {
+            window.clearInterval(i);
+        }
+        
         // set timer
-        periodEl.innerText = 1;
+        periodValue = 1
+        periodEl.innerText = periodValue;
         timerValue = timer();
     }
     
@@ -77,12 +105,14 @@ function startGame(){
 
     scoreHomeEl.textContent = homeCount;
     scoreGuestEl.textContent = guestCount;
-
 }
+
 
 // Timer function
 function timer(){
-    var sec = 30;
+
+    var sec = 5;
+
     var countdown = setInterval(function(){
         document.getElementById('timer').innerHTML='00:'+ sec.toString().padStart(2,"0");
         sec--;
@@ -93,18 +123,20 @@ function timer(){
 
             if (periodValue <4){
 
-                //reset timer
+                // start the timer for the new period
                 timer();
 
                 // set new period
                 periodValue += 1;
                 periodEl.innerText = periodValue;
-            } 
+            }
+            else{
+                // check who has won the game
+                win()
+            }
         }
 
     }, 1000);
-
-    console.log(countdown)
 
     return countdown
 }
